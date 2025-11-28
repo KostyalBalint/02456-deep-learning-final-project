f0a19816e29da46d0f98b1fd47d230a49658ec05
Tamás Tárkányi <tamas.tarkanyi@routepatch.com>
2025-11-28 05:27:43 +0100

    few-shot fast evaluation + masking methon choosing

--- few_shot/detect_anomalies.py
+++ few_shot/detect_anomalies.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def detect_anomalies(model, nn_index, test_image, use_masking=True,
+def detect_anomalies(model, nn_index, test_image, use_masking=True, masking_method='threshold',
                      k_neighbors=1, normalize_features=True):
 
     # Extract features from test image
@@ -10,10 +10,19 @@ def detect_anomalies(model, nn_index, test_image, use_masking=True,
 
     # Apply PCA masking with percentile threshold
     if use_masking:
-        mask = model.compute_background_mask(features, grid_size,
-                                            threshold_percentile=70,
-                                            masking_type=True,
-                                            smoothing_sigma=4)
+        if masking_method == 'threshold':
+            mask = model.compute_background_mask(features, grid_size,
+                                                threshold_percentile=70,
+                                                masking_type=False,
+                                                smoothing_sigma=4)
+        if masking_method == 'pca':
+            mask_grid, pc_map, dbg = get_foreground_mask_pca(
+            feats=features,
+            grid_size=grid_size,
+            debug=False,
+            return_debug=True,
+            )       
+            mask = (mask_grid.reshape(-1) > 0)
     else:
         mask = np.ones(features.shape[0], dtype=bool)
 
